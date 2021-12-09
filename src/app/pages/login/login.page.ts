dd230604d6baccf85f4273ece1d49ecf24b2ee54
doraemint <doraemintsmile@hotmail.com>
2021-12-10 03:56:50 +0700

    connect with firebase (login and signup)

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { Iuser } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  credentials = {} as Iuser;
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    let toaster = this.toastCtrl.create({
+      message: 'Error Please field ',
+      duration: 3000,
+      position: 'bottom'
+    });
+
+    this.authService.login(this.credentials).then((res: any) => {
+      if (!res.code) {
+        this.router.navigate(['signup']);
+      }
+    },
+      async (err) => {
+        (await toaster).message = 'Please try again' + err;
+        (await toaster).present();
+      });
+  }
+
+  gotoSignUp() {
+    this.router.navigate(['signup']);
   }
 
 }
--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  newUser = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  }
+
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController) { }
 
   ngOnInit() {
   }
 
+  async register() {
+    let toaster = this.toastCtrl.create({
+      message: 'Error Please field ',
+      duration: 3000,
+      position: 'bottom'
+    });
+
+    if (this.newUser.firstname == '' ||
+      this.newUser.lastname == '' ||
+      this.newUser.email == '' ||
+      this.newUser.password == '' ||
+      this.newUser.confirmPassword == '') {
+      (await toaster).message = 'All fields are require';
+      (await toaster).present();
+    } else if (this.newUser.password !== this.newUser.confirmPassword) {
+      (await toaster).message = "Passowrd don't match";
+      (await toaster).present();
+    } else if (this.newUser.password.length < 8) {
+      (await toaster).message = 'Passowrd should have more than 8 characters';
+      (await toaster).present();
+    } else {
+      let loader = await this.loadingCtrl.create({
+        message: 'Please wait'
+      });
+      loader.present();
+
+      this.userService.addUser(this.newUser).then((res: any) => {
+        loader.dismiss();
+        console.log('res', res)
+        this.router.navigate(['login']);
+      },
+        async (err) => {
+          loader.dismiss();
+          (await toaster).message = 'Please try again \n'+ err;
+          (await toaster).present();
+        });
+      };
+  }
+
+  goBackToLogin() {
+    this.router.navigate(['login']);
+  }
 }
